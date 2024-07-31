2177e5bc8c34fc6e60d5e324b9eedc05bd11c8db
Никиа <nikitka-smirnov-2013@mail.ru>
2024-08-01 01:18:59 +0300

    header

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,12 @@
-var _a;
-function sayHello() {
-    alert('Hello, World!');
-}
-(_a = document.getElementById("helloButton")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", sayHello);
+var Person = /** @class */ (function () {
+    function Person(name, age) {
+        this.name = name; // Инициализация свойства name
+        this.age = age; // Инициализация свойства age
+    }
+    Person.prototype.grrt = function () {
+        alert("Hello, my name is ".concat(this.name, " and I am ").concat(this.age, " years old."));
+    };
+    return Person;
+}());
+var person = new Person("John Doe", 30);
+person.grrt(); // Выводит: Hello, my name is John Doe and I am 30 years old.
--- scripts/index.ts
+++ scripts/index.ts
@@ -1,5 +1,17 @@
-function sayHello(): void {
-    alert('Hello, World!');
-}
-
-document.getElementById("helloButton")?.addEventListener("click", sayHello);
+class Person {
+    name: string;
+    age: number;
+  
+    constructor(name: string, age: number) {
+      this.name = name; // Инициализация свойства name
+      this.age = age; // Инициализация свойства age
+    }
+  
+    greet() {
+      alert(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+  }
+  
+  const person = new Person("John Doe", 30);
+  person.greet(); // Выводит: Hello, my name is John Doe and I am 30 years old.
+  
